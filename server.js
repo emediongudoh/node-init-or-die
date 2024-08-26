995f4644b64613fce0961bb9cc03ca26d466aca6
Emediong Udoh <emediong5135@gmail.com>
2024-08-26 10:28:10 +0100

    setup cross-env

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 // Create express app
 const app = express();
+
+// Load env variables
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const NODE_ENV = process.env.NODE_ENV;
 
 // Test route
 app.get(`/hello`, (req, res) => {
@@ -10,5 +17,5 @@ app.get(`/hello`, (req, res) => {
 
 // Start development server
 app.listen(8000, () => {
-    console.log(`Server listening on port 8000`);
+    console.log(`${NODE_ENV} server listening on port ${PORT}`);
 });
